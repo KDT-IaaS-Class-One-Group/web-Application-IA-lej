09d52784650b5d38da62456ec4cda5b19c979bbf
eunjeong <eunjeong990731@gmail.com>
2023-11-03 14:19:51 +0900

    서버파일 수정'

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-
+app.use(express.static('front'))
 app.get("/",(req,res)=>{
-    fs.readFile("./front/index.html",(err,data)=>{
+    fs.sendFild("/front/index.html",(err,data)=>{
         if(err){
-            res.writeHead(200,'Internal Server Error');
+            res.writeHead('Internal Server Error');
         } else {
             res.setHeader('Content-Type', 'text/html');
             res.send(data);
@@ -18,3 +18,4 @@ app.listen (PORT, () => {
       `cli 창에서 컨트롤 누른후  옆에 포트 누르면 편리하게 확인 -> http://localhost:${PORT}/`
     );
   });
+
