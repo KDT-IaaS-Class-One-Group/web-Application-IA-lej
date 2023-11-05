7217c3cf476ad5e2d84218e0a02e86fa98060e45
eunjeong <eunjeong990731@gmail.com>
2023-11-05 18:09:03 +0900

    라우팅 변경 및 메시지 표기 확인 할 수 있게 확인 가능

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 
-app.use(express.static('front'))
-app.get("/",(req,res)=>{
-    fs.sendFild("/front/index.html",(err,data)=>{
-        if(err){
-            res.writeHead('Internal Server Error');
-        } else {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(data);
-          } 
-})})
-const PORT = 8000;
-app.listen (PORT, () => {
-    console.log(
-      `cli 창에서 컨트롤 누른후  옆에 포트 누르면 편리하게 확인 -> http://localhost:${PORT}/`
-    );
+app.use(express.static('front'));
+app.use(express.json()); // JSON 파서 사용
+app.use(express.urlencoded({ extended: false }));
+
+const DATA_FILE = path.join(__dirname, 'data.json'); // 데이터를 저장할 JSON 파일
+
+// 초기화: 데이터 파일이 없으면 빈 객체로 초기화
+if (!fs.existsSync(DATA_FILE)) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify({ mainContent: { inputRecords: [] } }, null, 2));
+}
+
+// POST 요청 처리
+app.post('/message', (req, res) => {
+  const userMessage = req.body.message;
+  const timestamp = new Date().toLocaleTimeString();
+
+  fs.readFile(DATA_FILE, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+    jsonData.mainContent.inputRecords.push({
+      type: 'user',
+      message: userMessage,
+      timestamp: timestamp,
+    });
+
+    fs.writeFile(DATA_FILE, JSON.stringify(jsonData, null, 2), (err) => {
+      if (err) {
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.status(200).send('Message saved successfully');
+      }
+    });
   });
+});
 
+// GET 요청 처리
+app.get('/message', (req, res) => {
+  fs.readFile(DATA_FILE, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.status(200).json(JSON.parse(data));
+    }
+  });
+});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT}/ 에서 실행 중입니다.`);
+});
